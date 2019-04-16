c09df68812bd77baf408b80a0912ceebbfb27779
yangbiao <itbiaoyang@163.com>
2019-04-16 16:35:35 +0800

    新增了新手熟悉代码的调用链功能

--- src/main/java/com/yb/producer/service/ProducerServiceApplication.java
+++ src/main/java/com/yb/producer/service/ProducerServiceApplication.java
@@ -1,5 +1,6 @@
 package com.yb.producer.service;
 
+import cn.yueshutong.springprojecttree.config.annotation.EnableProjectTree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+//注解参数为pointcut表达式
+//方法调用链分析框架(非官方,某大神做的,给新手用来熟悉项目用的)
+@EnableProjectTree("execution(* com.yb.producer.service.controller..*(..))")
 public class ProducerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerServiceApplication.class, args);
     }
 
-    //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-    //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 }
