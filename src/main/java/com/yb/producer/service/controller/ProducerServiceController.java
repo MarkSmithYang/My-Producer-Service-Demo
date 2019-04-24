2686f9bb0962cc75bb440e8f79022538952fb1e9
yangbiao <itbiaoyang@163.com>
2019-04-24 20:02:18 +0800

    新增了新手熟悉代码的调用链功能a1

--- src/main/java/com/yb/producer/service/controller/ProducerServiceController.java
+++ src/main/java/com/yb/producer/service/controller/ProducerServiceController.java
@@ -3,6 +3,8 @@ package com.yb.producer.service.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,22 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  * author biaoyang
  * date 2019/4/10 001014:45
  */
+@RefreshScope
 @Api(tags = "我的服务提供接口")
 @RestController
 @CrossOrigin//处理跨域
 public class ProducerServiceController {
     //用于feign-ribbon-hystrix的服务接口
 
+    //@Value("${producer.test}")
+    private String test="哎哎哎";
+
     @ApiOperation("我的hello")
     @GetMapping("/hello")
     public String hello( @ApiParam(name = "name",value = "姓名") @RequestParam String name){
-        return "hello "+name;
+        return "hello "+name+"==="+test;
     }
 
     @ApiOperation("我的world")
     @GetMapping("/world")
     public String world(){
-        return "hello world----啊啊";
+        return "hello world----啊啊=-="+test;
     }
 
 }
